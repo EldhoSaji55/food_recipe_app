71d0b7f7b63a9ca89ef519a4ce0763184f4f24cb
EldhoSaji55 <eldhosaji4755@gmail.com>
2024-09-05 09:41:58 +0530

    added dummydb

---
+++ lib/dummy_db.dart
@@ -0,0 +1,104 @@
+class DummyDb {
+  static List trendingNowList = [
+    {
+      'rating': '4.5',
+      "duration": '15:10',
+      "imageurl":
+          'https://images.pexels.com/photos/1640777/pexels-photo-1640777.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+      "profileurl":
+          'https://images.pexels.com/photos/1310522/pexels-photo-1310522.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+      "title": 'How to make sushi at home',
+      "userName": 'Niki Samantha'
+    },
+    {
+      'rating': '4.2',
+      "duration": '12:05',
+      "imageurl":
+          'https://images.pexels.com/photos/376464/pexels-photo-376464.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+      "profileurl":
+          'https://images.pexels.com/photos/943084/pexels-photo-943084.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+      "title": 'Quick and Easy Pasta Recipes',
+      "userName": 'John Doe'
+    },
+    {
+      'rating': '4.8',
+      "duration": '18:30',
+      "imageurl":
+          'https://images.pexels.com/photos/70497/pexels-photo-70497.jpeg?auto=compress&cs=tinysrgb&w=600',
+      "profileurl":
+          'https://images.pexels.com/photos/1542085/pexels-photo-1542085.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+      "title": 'The Perfect Chocolate Cake',
+      "userName": 'Alice Johnson'
+    },
+    {
+      'rating': '3.9',
+      "duration": '09:45',
+      "imageurl":
+          'https://images.pexels.com/photos/1099680/pexels-photo-1099680.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+      "profileurl":
+          'https://images.pexels.com/photos/428364/pexels-photo-428364.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+      "title": 'Grilling Tips for Beginners',
+      "userName": 'Michael Smith'
+    },
+    {
+      'rating': '4.0',
+      "duration": '14:22',
+      "imageurl":
+          'https://images.pexels.com/photos/1279330/pexels-photo-1279330.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+      "profileurl":
+          'https://images.pexels.com/photos/91227/pexels-photo-91227.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+      "title": 'Healthy Breakfast Ideas',
+      "userName": 'Samantha Lee'
+    },
+    {
+      'rating': '4.7',
+      "duration": '16:08',
+      "imageurl":
+          'https://images.pexels.com/photos/958545/pexels-photo-958545.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+      "profileurl":
+          'https://images.pexels.com/photos/1516680/pexels-photo-1516680.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+      "title": 'Making Homemade Ice Cream',
+      "userName": 'Laura James'
+    },
+    {
+      'rating': '4.3',
+      "duration": '10:00',
+      "imageurl":
+          'https://images.pexels.com/photos/1640772/pexels-photo-1640772.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+      "profileurl":
+          'https://images.pexels.com/photos/614810/pexels-photo-614810.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+      "title": 'Vegetarian Dishes for Dinner',
+      "userName": 'James Patterson'
+    },
+    {
+      'rating': '5.0',
+      "duration": '20:15',
+      "imageurl":
+          'https://images.pexels.com/photos/1640774/pexels-photo-1640774.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+      "profileurl":
+          'https://images.pexels.com/photos/2381069/pexels-photo-2381069.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+      "title": 'BBQ Ribs with Special Sauce',
+      "userName": 'Chris Brown'
+    },
+    {
+      'rating': '4.1',
+      "duration": '11:50',
+      "imageurl":
+          'https://images.pexels.com/photos/769289/pexels-photo-769289.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+      "profileurl":
+          'https://images.pexels.com/photos/1438081/pexels-photo-1438081.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+      "title": 'French Toast for Breakfast',
+      "userName": 'Anna Bell'
+    },
+    {
+      'rating': '4.6',
+      "duration": '17:35',
+      "imageurl":
+          'https://images.pexels.com/photos/1092730/pexels-photo-1092730.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+      "profileurl":
+          'https://images.pexels.com/photos/3756679/pexels-photo-3756679.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+      "title": 'Classic Italian Pizza Making',
+      "userName": 'David Wilson'
+    },
+  ];
+}
